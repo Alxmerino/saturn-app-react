cebea26bc07659bd7a96080cbef098e9dc405229
Rene Merino <rene@amayamedia.com>
2022-04-26 23:28:03 -0400

    Initial action to add timer

--- src/components/Timer/TimerHeader/TimerHeader.tsx
+++ src/components/Timer/TimerHeader/TimerHeader.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Stack, TextField } from '@mui/material';
 import { Add } from '@mui/icons-material';
 
+import { useAppDispatch } from '../../../app/hooks';
+import { addTimer } from '../../../store/Timer/TimerSlice';
 import { Button, ProjectMenu } from '../../common';
 
 const TimerHeader = () => {
+  const dispatch = useAppDispatch();
+  const [title, setTitle] = useState<string>('');
+  const [plannedTime, setPlannedTime] = useState<string>('');
+  const [canAdd, setCanAdd] = useState<boolean>(false);
+
+  const handleOnTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    setCanAdd(event.target.value.length > 0);
+  };
+
+  const handlePlannedTimeChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPlannedTime(event.target.value);
+  };
+
+  const handleTimerAdd = () => {
+    dispatch(
+      addTimer({
+        title,
+        plannedTime,
+      })
+    );
+    setTitle('');
+    setPlannedTime('');
+    setCanAdd(false);
+  };
+
+  const onKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleTimerAdd();
+    }
+  };
+
   return (
     <Stack direction="row" pt={3} spacing={2}>
       <TextField
@@ -12,17 +48,23 @@ const TimerHeader = () => {
         size="small"
         fullWidth
         label="What are you working on?"
+        value={title}
+        onChange={handleOnTitleChange}
+        onKeyPress={onKeyPress}
       />
       <TextField
         id="timer-limit"
         size="small"
         label="For how long?"
+        value={plannedTime}
+        onChange={handlePlannedTimeChange}
+        onKeyPress={onKeyPress}
         sx={{
           width: 300,
         }}
       />
       <ProjectMenu color="primary" />
-      <Button type="primary">
+      <Button kind="primary" onClick={handleTimerAdd} disabled={!canAdd}>
         <Add />
       </Button>
     </Stack>
--- src/store/Timer/TimerSlice.ts
+++ src/store/Timer/TimerSlice.ts
@@ -1,21 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../store';
+import { TimerItem } from '../../types/types';
+
+// @todo: Add initial State from server?
+interface TimerState {
+  [x: number]: TimerItem[];
+}
+const initialState: TimerItem[] = [];
 
 export const TimerSlice = createSlice({
   name: 'timer',
-  // @todo: Add initial State from server?
-  initialState: {},
+  initialState,
   reducers: {
-    start: (state: any) => {
-      state.started = true;
-    },
-    stop: (state: any) => {
-      state.started = false;
+    addTimer(
+      state: TimerItem[],
+      action: PayloadAction<Pick<TimerItem, 'title' | 'plannedTime'>>
+    ) {
+      const { title, plannedTime } = action.payload;
+      const now: Date = new Date();
+      const nowTS: number = now.getTime();
+
+      const newTimer = {
+        // @todo: Add unique id
+        id: nowTS.toString(),
+        title: title,
+        running: false,
+        projectId: null,
+        // @todo: Get proper user id
+        userId: nowTS.toString(),
+        duration: null,
+        startTime: null,
+        endTime: null,
+        plannedTime: plannedTime,
+      };
+
+      state.push(newTimer);
     },
   },
   extraReducers: {},
 });
 
-export const { start, stop } = TimerSlice.actions;
+export const { addTimer } = TimerSlice.actions;
 export const selectTimer = (state: RootState) => state.timer;
 export default TimerSlice.reducer;
