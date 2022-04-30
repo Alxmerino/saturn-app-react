98e0f4e101495d3ce1a7aee01081391954ce4413
Rene Merino <rene@amayamedia.com>
2022-04-29 22:16:28 -0400

    Update timer slice

--- src/store/Timer/TimerSlice.ts
+++ src/store/Timer/TimerSlice.ts
@@ -1,36 +1,39 @@
-import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { groupBy } from 'lodash';
+
 import { RootState } from '../store';
-import { TimerItem } from '../../types/types';
+import { TimerItemTask } from '../../types/types';
 
 // @todo: Add initial State from server?
 interface TimerState {
-  [x: number]: TimerItem[];
+  [x: number]: TimerItemTask[];
 }
-const initialState: TimerItem[] = [];
+const initialState: TimerItemTask[] = [];
 
 export const TimerSlice = createSlice({
   name: 'timer',
   initialState,
   reducers: {
     addTimer(
-      state: TimerItem[],
-      action: PayloadAction<Pick<TimerItem, 'title' | 'plannedTime'>>
+      state: TimerItemTask[],
+      action: PayloadAction<Pick<TimerItemTask, 'title' | 'plannedTime'>>
     ) {
       const { title, plannedTime } = action.payload;
       const now: Date = new Date();
       const nowTS: number = now.getTime();
 
-      const newTimer = {
+      const newTimer: TimerItemTask = {
         // @todo: Add unique id
         id: nowTS.toString(),
         title: title,
         running: false,
-        projectId: null,
+        project: null,
         // @todo: Get proper user id
         userId: nowTS.toString(),
         duration: null,
         startTime: null,
         endTime: null,
+        createdAt: now,
         plannedTime: plannedTime,
       };
 
@@ -41,5 +44,11 @@ export const TimerSlice = createSlice({
 });
 
 export const { addTimer } = TimerSlice.actions;
-export const selectTimer = (state: RootState) => state.timer;
+export const selectTimers = (state: RootState) => state.timer;
+export const selectTimersByDate = (state: RootState) => {
+  return groupBy(state.timer, (timer) => {
+    // @todo: Better way to get date?
+    return timer.createdAt.getDay();
+  });
+};
 export default TimerSlice.reducer;
