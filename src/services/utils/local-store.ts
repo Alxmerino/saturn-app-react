94a0d5acd7d4d78d1639d51809abbfdd29debe2c
Rene Merino <rene@amayamedia.com>
2022-06-12 22:57:18 -0400

    Encode some local store values

--- src/services/utils/local-store.ts
+++ src/services/utils/local-store.ts
@@ -1,11 +1,18 @@
 const prefix = 'saturn_tt:';
 
 const LocalStore = {
-  get(key: string): any {
-    return JSON.parse(localStorage.getItem(prefix + key) ?? 'null');
+  get(key: string, encode = false): any {
+    const _key = encode ? btoa(key) : key;
+    const value = localStorage.getItem(prefix + _key) ?? 'null';
+
+    return JSON.parse(encode && value !== 'null' ? atob(value) : value);
   },
-  set(key: string, value: any): void {
-    localStorage.setItem(prefix + key, JSON.stringify(value));
+  set(key: string, value: any, encode = false): void {
+    const itemKey = encode ? btoa(key) : key;
+    const itemValue = encode
+      ? btoa(JSON.stringify(value))
+      : JSON.stringify(value);
+    localStorage.setItem(prefix + itemKey, itemValue);
   },
   remove(key: string): void {
     localStorage.removeItem(prefix + key);
