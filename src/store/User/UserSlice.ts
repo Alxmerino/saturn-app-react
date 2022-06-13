d04c7ed6fa8342c736e5a93ca9d1896cb2afb407
Rene Merino <rene@amayamedia.com>
2022-06-12 22:58:37 -0400

    Update UserSlice to store user info

--- src/store/User/UserSlice.ts
+++ src/store/User/UserSlice.ts
@@ -1,30 +1,65 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  CaseReducer,
+  createAsyncThunk,
+  createSlice,
+  PayloadAction,
+} from '@reduxjs/toolkit';
 import { RootState, AppThunk } from '../store';
+import { AuthResponse, User } from '../../types/types';
+import LocalStore from '../../services/utils/local-store';
 
 export interface UserState {
-  loggedIn: boolean;
+  isLoggedIn: boolean;
+  user: User | null;
+  token: string | null;
 }
 
 const initialState: UserState = {
-  loggedIn: true,
+  isLoggedIn: false,
+  user: null,
+  token: null,
 };
 
+const reducerName = 'auth';
+
+function getInitialState() {
+  const localState: UserState = LocalStore.get(reducerName, true);
+  return localState ?? initialState;
+}
+
 export const UserSlice = createSlice({
-  name: 'user',
-  initialState,
+  name: reducerName,
+  initialState: getInitialState(),
   reducers: {
-    login: (state) => {
-      state.loggedIn = true;
+    setLogin: (state: UserState) => {
+      state.isLoggedIn = true;
+
+      // Save to local storage
+      LocalStore.set(reducerName, state, true);
     },
-    logout: (state) => {
-      state.loggedIn = false;
+    setLogout: (state) => {
+      state.isLoggedIn = false;
+
+      // Save to local storage
+      LocalStore.set(reducerName, state, true);
+    },
+    setCredentials: (
+      state,
+      { payload: { user, token } }: PayloadAction<AuthResponse>
+    ) => {
+      state.user = user;
+      state.token = token;
+
+      // Save to local storage
+      LocalStore.set(reducerName, state, true);
     },
   },
   extraReducers: {},
 });
 
-export const { login, logout } = UserSlice.actions;
+export const { setCredentials, setLogin, setLogout } = UserSlice.actions;
 
-export const selectLoggedIn = (state: RootState) => state.user.loggedIn;
+export const selectCurrentUser = (state: RootState) => state.auth.user;
+export const selectLoggedIn = (state: RootState) => state.auth.isLoggedIn;
 
 export default UserSlice.reducer;
