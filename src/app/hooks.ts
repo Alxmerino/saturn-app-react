2dd85608893ba44f50176f988989f6ae7979ec3d
Rene Merino <rene@amayamedia.com>
2022-11-14 15:14:47 -0500

    Add timer hook

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -4,3 +4,6 @@ import { AppDispatch, RootState } from '../store/store';
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// Export additional hooks
+export * from './useTimer';
---
+++ src/app/useTimer.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useState } from 'react';
+import { DurationEntry, Task, TaskTimerItem } from '../types/timer';
+import { differenceInSeconds } from 'date-fns';
+import { durationInSecondsToString, getTotalDuration } from '../services/utils';
+
+const useTimer = (task: Task) => {
+  const { timers } = task;
+  const totalTaskDuration = useCallback(
+    () => getTimersDuration(timers),
+    [timers]
+  );
+  const activeTimer = timers.find((t) => t.running);
+  const [durationInSeconds, setDurationInSeconds] = useState(
+    activeTimer?.durationInSeconds ?? 0
+  );
+
+  useEffect(() => {
+    let interval: any;
+    if (activeTimer) {
+      interval = setInterval(() => {
+        const timeDiffInSeconds = differenceInSeconds(
+          Date.now(),
+          new Date(activeTimer.startTime ?? 0)
+        );
+
+        setDurationInSeconds(timeDiffInSeconds);
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [activeTimer]);
+
+  return {
+    durationInSeconds,
+    taskDurationInSeconds: totalTaskDuration(),
+    duration: durationInSecondsToString(durationInSeconds),
+    running: Boolean(activeTimer),
+    activeTimer,
+  };
+};
+
+const getTimersDuration = (timers: TaskTimerItem[]) => {
+  if (!timers.length) return 0;
+
+  return timers
+    .map(getTimerEntryDuration)
+    .reduce((prev, curr) => prev + curr, 0);
+};
+
+const getTimerEntryDuration = ({ startTime, endTime }: TaskTimerItem) => {
+  return differenceInSeconds(new Date(endTime ?? 0), new Date(startTime ?? 0));
+};
+
+export { useTimer };
