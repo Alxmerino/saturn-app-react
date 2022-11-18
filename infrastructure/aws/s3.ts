fc0b17362b79a766c42fe5428d05526270db6a5e
Rene Merino <rene@amayamedia.com>
2022-11-18 09:11:40 -0500

    Add method to create bucket policy

--- infrastructure/aws/s3.ts
+++ infrastructure/aws/s3.ts
@@ -2,7 +2,10 @@ import * as aws from '@pulumi/aws';
 import * as pulumi from '@pulumi/pulumi';
 
 export default class S3 extends pulumi.ComponentResource {
+  public readonly id: pulumi.Output<string>;
   public readonly arn: pulumi.Output<string>;
+  public readonly logsArn: pulumi.Output<string>;
+  public readonly logsDomainName: pulumi.Output<string>;
   public readonly name: pulumi.Output<string>;
   public readonly websiteUrl: pulumi.Output<string>;
 
@@ -24,26 +27,43 @@ export default class S3 extends pulumi.ComponentResource {
       tags,
     });
 
-    // Set the access policy for the bucket so all objects are readable
-    const bucketPolicy = new aws.s3.BucketPolicy(name + 'BucketPolicy', {
-      bucket: S3Bucket.id,
-      policy: S3Bucket.arn.apply((bucketArn) =>
-        JSON.stringify({
-          Version: '2012-10-17',
-          Statement: [
-            {
-              Effect: 'Allow',
-              Principal: '*',
-              Action: ['s3:GetObject'],
-              Resource: [`${bucketArn}/*`],
-            },
-          ],
-        })
-      ),
+    // Logs Bucket
+    const logsBucket = new aws.s3.Bucket('requestLogs', {
+      bucket: `${targetDomain}-logs`,
+      acl: 'private',
+      tags,
     });
 
+    this.id = S3Bucket.id;
     this.arn = S3Bucket.arn;
+    this.logsArn = logsBucket.arn;
+    this.logsDomainName = logsBucket.bucketDomainName;
     this.name = S3Bucket.bucket;
     this.websiteUrl = S3Bucket.websiteEndpoint;
+  }
+
+  createBucketPolicy(args: any) {
+    const { originAccessIdentityIamArn } = args;
+    // Set the access policy for the bucket so all objects are readable
+    const bucketPolicy = new aws.s3.BucketPolicy(this.name + 'BucketPolicy', {
+      bucket: this.id,
+      policy: pulumi
+        .all([originAccessIdentityIamArn, this.arn])
+        .apply(([oaiArn, bucketArn]) =>
+          JSON.stringify({
+            Version: '2012-10-17',
+            Statement: [
+              {
+                Effect: 'Allow',
+                Principal: {
+                  AWS: oaiArn,
+                }, // Only allow Cloudfront read access.
+                Action: ['s3:GetObject'],
+                Resource: [`${bucketArn}/*`], // Give Cloudfront access to the entire bucket.
+              },
+            ],
+          })
+        ),
+    });
   }
 }
