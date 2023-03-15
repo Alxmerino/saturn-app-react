7af9da39a9101073c40521a03ec00eb6c20b976e
Rene Merino <rene@amayamedia.com>
2023-03-15 07:43:21 -0400

    Runs back simple tauri app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,8 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{utils::config::AppUrl, window::WindowBuilder, WindowUrl};
-
 fn main() {
-  let port = portpicker::pick_unused_port().expect("failed to find unused port");
-
-  let mut context = tauri::generate_context!();
-  let url = format!("http://localhost:{}", port).parse().unwrap();
-  let window_url = WindowUrl::External(url);
-  // rewrite the config so the IPC is enabled on this URL
-  context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
-  context.config_mut().build.dev_path = AppUrl::Url(window_url.clone());
-
   tauri::Builder::default()
-    .plugin(tauri_plugin_localhost::Builder::new(port).build())
-    .setup(move |app| {
-      WindowBuilder::new(app, "main".to_string(), window_url)
-        .title("Saturn Time Tracker")
-        .build()?;
-      Ok(())
-    })
-    .run(context)
+    .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
